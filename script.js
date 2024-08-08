eff1af133c359b6d34a70ade4e9f69bc1d0111f7
Manmohankhairwar <manmohankhairwar369@gmail.com>
2024-08-08 18:29:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const formContainer = document.querySelector(".form-container");
+    formContainer.style.transform = "translateY(-100px)";
+    formContainer.style.opacity = "0";
+    formContainer.style.animation = "fadeIn 1.5s ease-in-out forwards";
+});
